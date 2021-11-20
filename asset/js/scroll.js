f5e23c3c421cf58c430a91e6cbb6658a318039c2
Man From Nowhere <manfnowhere24@gmail.com>
2021-11-20 23:16:41 +0100

    TES LAYAR LEBAR

--- asset/js/scroll.js
+++ asset/js/scroll.js
@@ -1,25 +1,25 @@
 const faders = document.querySelectorAll(".fade-in");
-        const sliders = document.querySelectorAll(".slide-in ");
+const sliders = document.querySelectorAll(".slide-in ");
     
-        const appearOptions = { 
-            threshold: 1, rootMargin: "10px 0px -20px 0px" };
+const appearOptions = { 
+    threshold: 1, rootMargin: "0px 0px -100px 0px" };
     
-        const appearOnScroll = new IntersectionObserver(
-            function(
-                enteries, 
-                appearOnScroll
-                ) {
-                    enteries.forEach(entry => {
-                        if (!entry.isIntersecting){
-                            return;
-                        }else{
-                            entry.target.classList.add("appear");
-                            appearOnScroll.unobserve(entry.target);
-                        }
-                    })
-                }, 
-                appearOptions);
+const appearOnScroll = new IntersectionObserver(
+    function(
+        enteries, 
+        appearOnScroll
+    ) {
+        enteries.forEach(entry => {
+            if (!entry.isIntersecting){
+                return;
+            }else{
+                entry.target.classList.add("appear");
+                appearOnScroll.unobserve(entry.target);
+            }
+        })
+    }, 
+    appearOptions);
     
-            faders.forEach(fader => {
-            appearOnScroll.observe(fader)
-            });
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader)
+    });
